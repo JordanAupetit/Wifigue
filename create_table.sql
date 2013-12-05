7c43bc54662f815f1525594e797a34f590b75a8c
Paul Hally <pauhally@emi.u-bordeaux1.fr>
2013-12-05 22:52:28 +0100

    base Sql done

---
+++ create_table.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Produit
+(
+	idProduit INTEGER AUTO_INCREMENT,
+	nomProduit VARCHAR(30),
+	description VARCHAR(1000),
+	prixProduit INTEGER,
+	primary key(idProduit)
+	
+);
+CREATE TABLE Tag
+(
+	idTag INTEGER AUTO_INCREMENT,
+	nomTag VARCHAR(100),
+	primary key(idTag)
+);
+CREATE TABLE Affect
+(
+	idTag INTEGER,
+	idProduit INTEGER,
+	primary key(idTag,idProduit)
+	
+);
+CREATE TABLE Question
+(
+	idQuestion INTEGER AUTO_INCREMENT,
+	nomQuestion VARCHAR(100),
+	primary key(idQuestion)
+);
+CREATE TABLE Reponse
+(
+-- idReponse 5 pourrait etre par exemple la saisie d'un entier pour un prix
+	idReponse INTEGER AUTO_INCREMENT,
+	nomReponse VARCHAR(100),
+	primary key(idReponse)
+);
+CREATE TABLE ReponseQuestion
+(
+	idQuestion INTEGER,
+	idReponse INTEGER,
+	primary key(idQuestion,idReponse)
+);
+
+
+
---
+++ modele/connexionbase.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+//Fichier servant à la connexion.
+
+define('SERVER',"localhost");
+define('LOGIN',"root");
+define('PASSWORD',"");
+define('DATABASE',"reseau");
+
+$connexion=mysql_connect(SERVER,LOGIN,PASSWORD);
+
+if(!$connexion)
+{
+	echo "Cant connect to ".SERVER."\n";
+	exit;
+}
+
+mysql_select_db(DATABASE,$connexion) or die("Cant switch to".DATABASE."\n");
+
+?>
