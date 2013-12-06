7d72b18f7df61382da4c10af4baba65b11a5daee
jordan <aupetit.jordan@gmail.com>
2013-12-06 02:21:08 +0100

    Réajustement du Git

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'dbserver';
-$db['default']['username'] = 'pauhally';
-$db['default']['password'] = 'wifiguesbase';
-$db['default']['database'] = 'pauhally';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
